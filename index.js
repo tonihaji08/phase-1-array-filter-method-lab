b5fd3d5c68853a34ae77f4cd75e0d9e2ed63f948
Your Name <antony.adriano@stundent.moringaschool.com>
2022-11-27 17:00:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, string) {
+    return drivers.filter(drivers => drivers.toLowerCase() === string.toLowerCase())
+
+}
+
+//function takes an array of drivers' names and a string as arguments for querying the array, and returns all drivers whose names begin with the provided letters.
+
+function fuzzyMatch(drivers, string){
+    return drivers.filter(drivers => drivers.startsWith(string))
+
+}
+
+//function takes an array of driver objects and a string as arguments. Each driver object has two properties: name and hometown. The function should return each element whose name property matches the provided string argument.
+
+function matchName (drivers, string) {
+    return drivers.filter( drivers => drivers.name === string)
+}
